4f45a2c9b1bf7b38ba5fa6f0d0a038ca02650bca
bogdan <bogdan4888@gmail.com>
2023-05-30 17:54:38 +0300

    UC#1 Infrastructure setup

---
+++ src/main/java/com/bogdan/projectWithAI/ProjectWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.bogdan.projectWithAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWithAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bogdan/projectWithAI/ProjectWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogdan.projectWithAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
